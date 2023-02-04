0dccf669717b21ea2e1434781e2b041c6cead85f
parkchunsu <pcc0105@gmail.com>
2023-02-04 13:57:49 +0900

    sec09

--- javascript/dom/javascript1-dom.js
+++ javascript/dom/javascript1-dom.js
@@ -1,3 +1,59 @@
+//Ex10-클릭한 컬럼을 기준으로 레코드 정렬하기 #1
+window.addEventListener("load", function() {
+    var notices = [
+        {"id":1, "title":"유투브에 끌려다니지 않으려고 했는데....ㅜㅜ..", "regDate":"2019-02-05", "writerId":"newlec", "hit":2},
+        {"id":2, "title":"자바스크립트란..", "regDate":"2019-02-02", "writerId":"newlec", "hit":0},
+        {"id":3, "title":"기본기가 튼튼해야....", "regDate":"2019-02-01", "writerId":"newlec", "hit":1},
+        {"id":4, "title":"근데 조회수가 ㅜㅜ..", "regDate":"2019-01-25", "writerId":"newlec", "hit":0}
+    ];
+
+    var section = document.querySelector("#section10");
+    var titleId = document.querySelector(".title");
+    var noticeList =section.querySelector(".notice-list"); 
+    var tbodyNode = noticeList.querySelector("tbody");
+
+    var bindData = function() {
+        var template = section.querySelector("template");
+        for (var i = 0; i < notices.length; i++){
+            var cloneNode = document.importNode(template.content, true);
+            var tds = cloneNode.querySelectorAll("td");
+            tds[0].textContent = notices[i].id;
+            tds[1].innerHTML = '<a href="">' + notices[i].title; + '</a>'
+            tds[2].textContent = notices[i].regDate;
+            tds[3].textContent = notices[i].writerId;
+            tds[4].textContent = notices[i].hit;
+    
+            tbodyNode.append(cloneNode);
+        } 
+    };
+    bindData();
+
+    var titleSorted = false;
+
+    titleId.onclick = function(){
+        tbodyNode.innerHTML = "";
+        if(!titleSorted){
+            notices.sort(function(a,b){
+                titleSorted = true;
+                if(a.title < b.title){
+                    return -1;
+                } else if(a.title > b.title) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            });
+        } else{
+            notices.reverse();
+        }
+        bindData();
+
+       
+    };
+
+    
+});
+
 //Ex9 : 다중 엘리먼트 선택방법과 일괄 삭제
 window.addEventListener("load", function() {
     var section = document.querySelector("#section9");
